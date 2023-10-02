baa5f35c7d7603a39e2345a30573f5f6b9a742d7
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-10-02 16:10:42 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -3,3 +3,14 @@ sudo yum update -y
 amazon-linux-extras install nginx1 -y
 systemctl enable nginx
 systemctl start nginx
+
+##############################################
+
+"echo 'Hello from the remote instance'",
+"sudo yum update", # Update package list for linux.
+"sudo yum install -y python3-pip", # Package installation.
+"cd /tmp/script.sh",
+"sudo pip3 install flask",
+"sudo python3 script.sh",
+
+
